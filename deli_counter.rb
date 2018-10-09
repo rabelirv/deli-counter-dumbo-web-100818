d098f38f10a3cd55443500edba7423b73d641f18
Irving E Rabel <rabel606@gmail.com>
2018-10-09 18:58:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def line(katz_deli)
+    counter =0
+    katz_deli.each do |person|
+      
+end 
+
+def take_a_number(katz_deli, string)
+if katz_deli == []
+katz_deli << string
+counter = 1 
+katz_deli.each {|person| puts "Welcome, #{person}. You are number #{counter} in line."
+counter +=1}
+elsif katz_deli == ["Logan", "Avi", "Spencer"]
+katz_deli<< string 
+counter = 4
+puts "Welcome, #{string}. You are number #{counter} in line."
+end 
+end 
